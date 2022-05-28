c0dd79a666b5beb54c1b6e9bdfa72092cc1b464d
HariVP03 <harryskotch11@gmail.com>
2022-05-28 20:36:43 +0530

    fixed width change in login signup

--- apps/web/pages/auth/login.tsx
+++ apps/web/pages/auth/login.tsx
@@ -27,7 +27,7 @@ export default function SimpleCard() {
             justify={"center"}
             bg={useColorModeValue("gray.50", "gray.800")}
         >
-            <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack spacing={8} mx={"auto"} w={"md"} py={12} px={6}>
                 <Stack align={"center"}>
                     <Heading fontSize={"4xl"}>Sign in to your account</Heading>
                 </Stack>
--- apps/web/pages/auth/signup.tsx
+++ apps/web/pages/auth/signup.tsx
@@ -39,7 +39,7 @@ export default function SignupCard() {
             justify={"center"}
             bg={useColorModeValue("gray.50", "gray.800")}
         >
-            <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack spacing={8} mx={"auto"} w={"md"} py={12} px={6}>
                 <Stack align={"center"}>
                     <Heading fontSize={"4xl"} textAlign={"center"}>
                         Sign up
